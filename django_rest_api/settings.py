7c5dbb0740284c15f1186a00c893aace57f32c09
Khen Paler <paler.khen.ustp@gmail.com>
2025-04-28 15:31:27 +0800

    setup auth and employee

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Add the apps directory to Python path
+sys.path.insert(0, os.path.join(BASE_DIR))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Project apps
+    'apps.authentication',
+    'apps.employees',
 ]
 
 MIDDLEWARE = [
